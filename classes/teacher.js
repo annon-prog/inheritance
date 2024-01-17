49260b7bed6cbbb2b8262b189d1d74b64285c1ae
annon-prog <fidelisdela.wambui@gmail.com>
2024-01-18 00:53:41 +0300

    Task:Final submission

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,26 @@
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let years = [];
+
+    teachers.forEach(teacher => {
+      years.push(teacher.yearsOfExperience);
+    });
+
+    let sumOfYears = years.reduce(
+      (accum, currentValue) => accum + currentValue, 
+      0);
+    return sumOfYears;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
